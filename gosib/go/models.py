9185a0e940c92257f7dfe37b31e7ffb397fffae3
stillinbeta <web@stillinbeta.com>
2012-03-24 22:44:57 +1100

    Added model methods

--- gosib/go/models.py
+++ gosib/go/models.py
@@ -1,8 +1,31 @@
+from random import randint
+
 from django.db import models
 
+
 # Create your models here.
+
+class NounManager(models.Manager):
+    def shorten(self, url):
+        elements = self.filter(active=False).count()
+        if not elements:
+            raise Noun.DoesNotExist()
+        random_idx = randint(0, elements - 1)
+        noun = self.filter(active=False)[random_idx]
+        noun.url = url
+        noun.active = True
+        noun.save()
+        
+        return noun
 
 class Noun(models.Model):
     noun = models.SlugField(unique=True)
     url = models.URLField(blank=True)
     active = models.BooleanField(default=False)
+
+    def follow(self):
+        self.url = ""
+        self.active = False
+        self.save()
+
+    objects = NounManager()
--- gosib/go/tests.py
+++ gosib/go/tests.py
@@ -1,16 +1,31 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
 
+from models import Noun
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class NounTestCase(TestCase):
+    def setUp(self):
+        self.noun0 = Noun.objects.create(noun='potato')
+        
+    def test_shorten(self):
+        url ='http://stillinbeta.com' 
+        noun = Noun.objects.shorten(url)
+        self.assertTrue(noun.active)
+        self.assertEquals(noun.url, url)
+    
+        # Since there should only be one noun, this should fail 
+        with self.assertRaises(Noun.DoesNotExist):
+            Noun.objects.shorten(url)
+
+        noun.active = False
+        noun.url = ""
+        noun.save()
+
+    def test_follow(self):
+        self.noun0.active = True
+        self.noun0.url = 'http://google.com'
+        self.noun0.save()
+
+        self.noun0.follow()
+
+        self.assertEquals(self.noun0.url, "")
+        self.assertFalse(self.noun0.active)
