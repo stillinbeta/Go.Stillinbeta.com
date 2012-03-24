f5eb86d50232e1cfde5e31381b42794ca212a83a
stillinbeta <web@stillinbeta.com>
2012-03-25 02:47:34 +1100

    Made some toast!

--- android/src/com/stillinbeta/go/GoSibActivity.java
+++ android/src/com/stillinbeta/go/GoSibActivity.java
@@ -1,7 +1,12 @@
 package com.stillinbeta.go;
 
+import android.content.Context;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Toast;
+import android.widget.Button;
 
 public class GoSibActivity extends Activity
 {
@@ -11,5 +16,16 @@ public class GoSibActivity extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        Button shorten = (Button)findViewById(R.id.shorten);
+        shorten.setOnClickListener(sendUrl);
+
     }
+
+    private OnClickListener sendUrl = new OnClickListener() {
+        public void onClick(View v) {
+            Context context = getApplicationContext();
+            Toast toast = Toast.makeText(context, "OHAI!", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+    };
 }
